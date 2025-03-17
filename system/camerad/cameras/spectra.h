dcbc1c51e30df95e9f6c6a91fd424304df4149dc
SangJo Kim <kandrea@nate.com>
2025-03-17 09:13:46 +0900

    25-03-17

--- system/camerad/cameras/spectra.h
+++ system/camerad/cameras/spectra.h
@@ -22,7 +22,6 @@ const int MIPI_SETTLE_CNT = 33;  // Calculated by camera_freqs.py
 // For use with the Titan 170 ISP in the SDM845
 // https://github.com/commaai/agnos-kernel-sdm845
 
-
 // CSLDeviceType/CSLPacketOpcodesIFE from camx
 // cam_packet_header.op_code = (device << 24) | (opcode);
 #define CSLDeviceTypeImageSensor (0x01 << 24)
@@ -30,12 +29,6 @@ const int MIPI_SETTLE_CNT = 33;  // Calculated by camera_freqs.py
 #define CSLDeviceTypeBPS         (0x10 << 24)
 #define OpcodesIFEInitialConfig  0x0
 #define OpcodesIFEUpdate         0x1
-
-typedef enum {
-  ISP_RAW_OUTPUT,   // raw frame from sensor
-  ISP_IFE_PROCESSED,  // fully processed image through the IFE
-  ISP_BPS_PROCESSED,  // fully processed image through the BPS
-} SpectraOutputType;
 
 std::optional<int32_t> device_acquire(int fd, int32_t session_handle, void *data, uint32_t num_resources=1);
 int device_config(int fd, int32_t session_handle, int32_t dev_handle, uint64_t packet_handle);
@@ -117,7 +110,7 @@ public:
 
 class SpectraCamera {
 public:
-  SpectraCamera(SpectraMaster *master, const CameraConfig &config, SpectraOutputType out);
+  SpectraCamera(SpectraMaster *master, const CameraConfig &config);
   ~SpectraCamera();
 
   void camera_open(VisionIpcServer *v, cl_device_id device_id, cl_context ctx);
@@ -188,11 +181,10 @@ public:
   int sync_objs_ife[MAX_IFE_BUFS] = {};
   int sync_objs_bps[MAX_IFE_BUFS] = {};
   uint64_t request_id_last = 0;
+  uint64_t last_requeue_ts = 0;
   uint64_t frame_id_raw_last = 0;
   int invalid_request_count = 0;
   bool skip_expected = true;
-
-  SpectraOutputType output_type;
 
   CameraBuf buf;
   SpectraMaster *m;
@@ -211,11 +203,16 @@ private:
   inline static bool first_frame_synced = false;
 
   // a mode for stressing edge cases: realignment, sync failures, etc.
-  inline bool stress_test(const char* log) {
-    static double prob = std::stod(util::getenv("SPECTRA_ERROR_PROB", "-1"));;
-    bool triggered = (prob > 0) && ((static_cast<double>(rand()) / RAND_MAX) < prob);
+  inline bool stress_test(std::string log) {
+    static double last_trigger = 0;
+    static double prob = std::stod(util::getenv("SPECTRA_ERROR_PROB", "-1"));
+    static double dt = std::stod(util::getenv("SPECTRA_ERROR_DT", "1"));
+    bool triggered = (prob > 0) && \
+                     ((static_cast<double>(rand()) / RAND_MAX) < prob) && \
+                     (millis_since_boot() - last_trigger) > dt;
     if (triggered) {
-      LOGE("stress test (cam %d): %s", cc.camera_num, log);
+      last_trigger = millis_since_boot();
+      LOGE("stress test (cam %d): %s", cc.camera_num, log.c_str());
     }
     return triggered;
   }
